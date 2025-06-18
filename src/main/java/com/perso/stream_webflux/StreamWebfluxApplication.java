cebd6aa533ea06d4d3348f5a76572ed84e0e9b04
Houssem <houssem.dhifaoui94@gmail.com>
2025-06-18 15:37:11 +0200

    init socle

---
+++ src/main/java/com/perso/stream_webflux/StreamWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.perso.stream_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perso/stream_webflux/StreamWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perso.stream_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
